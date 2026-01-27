46ffb0412219f752f000c5e4e5ad05aac6ad95d4
Marin Atanasov Nikolov <dnaeon@gmail.com>
2026-01-27 10:01:27 +0200

    apis/config/validation: validate spec of the gRPC exporter

--- pkg/apis/config/validation/validation.go
+++ pkg/apis/config/validation/validation.go
@@ -21,6 +21,7 @@ func Validate(cfg config.CollectorConfig) error {
 	anyExporterEnabled := []bool{
 		cfg.Spec.Exporters.DebugExporter.IsEnabled(),
 		cfg.Spec.Exporters.OTLPHTTPExporter.IsEnabled(),
+		cfg.Spec.Exporters.OTLPGRPCExporter.IsEnabled(),
 	}
 
 	if !cmp.Or(anyExporterEnabled...) {
@@ -36,23 +37,23 @@ func Validate(cfg config.CollectorConfig) error {
 		value string
 	}{
 		{
-			path:  "spec.exporters.otlphttp.endpoint",
+			path:  "spec.exporters.otlp_http.endpoint",
 			value: cfg.Spec.Exporters.OTLPHTTPExporter.Endpoint,
 		},
 		{
-			path:  "spec.exporters.otlphttp.traces_endpoint",
+			path:  "spec.exporters.otlp_http.traces_endpoint",
 			value: cfg.Spec.Exporters.OTLPHTTPExporter.TracesEndpoint,
 		},
 		{
-			path:  "spec.exporters.otlphttp.metrics_endpoint",
+			path:  "spec.exporters.otlp_http.metrics_endpoint",
 			value: cfg.Spec.Exporters.OTLPHTTPExporter.MetricsEndpoint,
 		},
 		{
-			path:  "spec.exporters.otlphttp.logs_endpoint",
+			path:  "spec.exporters.otlp_http.logs_endpoint",
 			value: cfg.Spec.Exporters.OTLPHTTPExporter.LogsEndpoint,
 		},
 		{
-			path:  "spec.exporters.otlphttp.profiles_endpoint",
+			path:  "spec.exporters.otlp_http.profiles_endpoint",
 			value: cfg.Spec.Exporters.OTLPHTTPExporter.ProfilesEndpoint,
 		},
 	}
@@ -69,17 +70,27 @@ func Validate(cfg config.CollectorConfig) error {
 	}
 
 	// Make sure that the HTTP client read/write buffers are good
-	nonNegativeFields := []struct {
+	type nonNegativeField struct {
 		path  string
 		value int
-	}{
+	}
+
+	nonNegativeFields := []nonNegativeField{
 		{
-			path:  "spec.exporters.otlphttp.read_buffer_size",
+			path:  "spec.exporters.otlp_http.read_buffer_size",
 			value: cfg.Spec.Exporters.OTLPHTTPExporter.ReadBufferSize,
 		},
 		{
-			path:  "spec.exporters.otlphttp.write_buffer_size",
+			path:  "spec.exporters.otlp_http.write_buffer_size",
 			value: cfg.Spec.Exporters.OTLPHTTPExporter.WriteBufferSize,
+		},
+		{
+			path:  "spec.exporters.otlp_grpc.read_buffer_size",
+			value: cfg.Spec.Exporters.OTLPGRPCExporter.ReadBufferSize,
+		},
+		{
+			path:  "spec.exporters.otlp_grpc.write_buffer_size",
+			value: cfg.Spec.Exporters.OTLPGRPCExporter.WriteBufferSize,
 		},
 	}
 
@@ -100,25 +111,49 @@ func Validate(cfg config.CollectorConfig) error {
 
 	resourceRefs := []resourceRef{
 		{
-			path: "spec.exporters.otlphttp.token",
+			path: "spec.exporters.otlp_http.token",
 			ref:  cfg.Spec.Exporters.OTLPHTTPExporter.Token,
+		},
+		{
+			path: "spec.exporters.otlp_grpc.token",
+			ref:  cfg.Spec.Exporters.OTLPGRPCExporter.Token,
 		},
 	}
 
+	// Referenced resources from the OTLP HTTP exporter
 	if cfg.Spec.Exporters.OTLPHTTPExporter.TLS != nil {
 		resourceRefs = append(
 			resourceRefs,
 			resourceRef{
-				path: "spec.exporters.otlphttp.tls.ca",
+				path: "spec.exporters.otlp_http.tls.ca",
 				ref:  cfg.Spec.Exporters.OTLPHTTPExporter.TLS.CA,
 			},
 			resourceRef{
-				path: "spec.exporters.otlphttp.tls.cert",
+				path: "spec.exporters.otlp_http.tls.cert",
 				ref:  cfg.Spec.Exporters.OTLPHTTPExporter.TLS.Cert,
 			},
 			resourceRef{
-				path: "spec.exporters.otlphttp.tls.key",
+				path: "spec.exporters.otlp_http.tls.key",
 				ref:  cfg.Spec.Exporters.OTLPHTTPExporter.TLS.Key,
+			},
+		)
+	}
+
+	// Referenced resources from the OTLP gRPC exporter
+	if cfg.Spec.Exporters.OTLPGRPCExporter.TLS != nil {
+		resourceRefs = append(
+			resourceRefs,
+			resourceRef{
+				path: "spec.exporters.otlp_grpc.tls.ca",
+				ref:  cfg.Spec.Exporters.OTLPGRPCExporter.TLS.CA,
+			},
+			resourceRef{
+				path: "spec.exporters.otlp_grpc.tls.cert",
+				ref:  cfg.Spec.Exporters.OTLPGRPCExporter.TLS.Cert,
+			},
+			resourceRef{
+				path: "spec.exporters.otlp_grpc.tls.key",
+				ref:  cfg.Spec.Exporters.OTLPGRPCExporter.TLS.Key,
 			},
 		)
 	}
@@ -131,6 +166,32 @@ func Validate(cfg config.CollectorConfig) error {
 					field.Invalid(field.NewPath(f.path), f.path, "name or dataKey is empty"),
 				)
 			}
+		}
+	}
+
+	// Validate expected string values are not empty
+	type nonEmptyString struct {
+		path  string
+		value string
+	}
+
+	nonEmptyStrings := make([]nonEmptyString, 0)
+	if cfg.Spec.Exporters.OTLPGRPCExporter.IsEnabled() {
+		nonEmptyStrings = append(
+			nonEmptyStrings,
+			nonEmptyString{
+				path:  "spec.exporters.otlp_grpc.endpoint",
+				value: cfg.Spec.Exporters.OTLPGRPCExporter.Endpoint,
+			},
+		)
+	}
+
+	for _, f := range nonEmptyStrings {
+		if f.value == "" {
+			allErrs = append(
+				allErrs,
+				field.Invalid(field.NewPath(f.path), f.path, "empty value specified"),
+			)
 		}
 	}
 
